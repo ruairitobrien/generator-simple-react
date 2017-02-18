92ec56c06ac5e487ee20b2321e1ae331487c7bcf
Ruairi O'Brien <ruairitobrien@spotify.com>
2017-02-18 11:40:59 +0100

    Updated repo in package.json. Fixed name of HomePage class.

--- generators/app/templates/src/home/HomePage.jsx
+++ generators/app/templates/src/home/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class HomeComponent extends React.Component {
+export default class HomePage extends React.Component {
     render() {
         return (
             <div>
